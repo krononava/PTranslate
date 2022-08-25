8d364f3a4d3a40a70c82403c26e51a7298785b70
lenix3347 <lenix3347@gmail.com>
2022-08-25 19:42:33 +0800

    Initial commit

---
+++ PTranslation.py
@@ -0,0 +1,52 @@
+from tkinter import *
+from googletrans import Translator
+
+translator = Translator()
+
+counter = False
+default = False
+mode = 'en'
+
+def trans(event, mode):
+    global counter
+    words = entry.get()
+    result = translator.translate(words, src='ms', dest=mode).text
+    entry.delete(0, END)
+    entry.insert(string=result, index=0)
+    counter = True
+
+def ontop():
+    window.lift()
+    window.attributes('-topmost', 1)
+    window.attributes('-topmost', 0)
+    entry.focus_force()
+
+def clear_field(event):
+    global counter
+    if counter == True:
+        entry.delete(0, END)
+        entry.insert(string=event.char, index=0)
+        counter = False
+
+def change_mode():
+    global mode
+    global default
+    default = not default 
+    if default == True:
+        mode = 'zh-CN'
+        button.configure(text="华语")
+    else:
+        mode = 'en'
+        button.configure(text="English")
+
+window = Tk()
+window.title("PTranslate")
+entry = Entry(fg="white", bg="black", width=37, font=(None, 18), justify='center')
+button = Button(text="English", width=44, height=1, bg="purple", fg="white", command=lambda: change_mode())
+window.bind("<Return>", lambda event: trans(event, mode)) 
+window.bind("<Key>", lambda event: clear_field(event))
+
+entry.pack()
+button.pack()
+ontop()
+window.mainloop()
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# PTranslate
+## PTranslate
+
+Inspired by [QTranslate](https://quest-app.appspot.com/) which is no longer working after upgrading windows 11.
+
+Use cases are the following:
+- translate Bahasa Melayu to English/Chinese 
+- saves time by not needing to go to [google translate](https://translate.google.com/) website
+
+## How to use?
+
+Start typing any BM words and hit enter. Click on the purple button to change between English/Chinese translation.
